25f441761c68ae6ef308790411453ffccaa8474a
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2025-08-26 23:47:34 -0700

    Update workset2_generation_script.py

--- pilot_study_worksets/workset2_100_poems/workset2_generation_script.py
+++ pilot_study_worksets/workset2_100_poems/workset2_generation_script.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-VOLUME_DIR = 'Volumes'
-OUTPUT_DIR = os.path.join('MyPoemAssignments', 'Collection')
+VOLUME_DIR = 'full_corpus'
+OUTPUT_DIR = os.path.join('workset2', 'collection')
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 os.makedirs(os.path.join(OUTPUT_DIR, 'mdp.39015029858522_071'), exist_ok=True)
